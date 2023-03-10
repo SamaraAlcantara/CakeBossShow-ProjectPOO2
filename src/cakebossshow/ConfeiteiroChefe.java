32649fc6e155807485ece53e37119643d75536c2
IaraFV <iaraferrevit2@gmail.com>
2023-03-09 21:09:34 -0300

    Adicionando implementações para o padrão Strategy

--- src/cakebossshow/ConfeiteiroChefe.java
+++ src/cakebossshow/ConfeiteiroChefe.java
@@ -10,5 +10,11 @@ public class ConfeiteiroChefe {
         
     }
     
+    public void ingredientes(){
+        String Chantilly = "Chantilly "; 
+        String PastaAmericana = "Pasta americana";
+        String MousseDeChocolate = "MousseDeChocolate";
+        String RecheioDePistache = "RecheioDePistache"; 
+    }
     
 }

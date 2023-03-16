341efe257012937b452b04e7c82bcbe73f5fdf3e
IaraFV <iaraferrevit2@gmail.com>
2023-03-15 21:09:50 -0300

    iniciando o padrao Decorator

---
+++ src/cakebossshow/Ingredientes.java
@@ -0,0 +1,32 @@
+package cakebossshow;
+
+public abstract class Ingredientes {
+    String Chantilly; 
+    public String getChantilly() {
+        return Chantilly;
+    }
+    public void setChantilly(String chantilly) {
+        Chantilly = chantilly;
+    }
+    String PastaAmericana;
+    public String getPastaAmericana() {
+        return PastaAmericana;
+    }
+    public void setPastaAmericana(String pastaAmericana) {
+        PastaAmericana = pastaAmericana;
+    }
+    String MousseDeChocolate;
+    public String getMousseDeChocolate() {
+        return MousseDeChocolate;
+    }
+    public void setMousseDeChocolate(String mousseDeChocolate) {
+        MousseDeChocolate = mousseDeChocolate;
+    }
+    String RecheioDePistache;
+    public String getRecheioDePistache() {
+        return RecheioDePistache;
+    }
+    public void setRecheioDePistache(String recheioDePistache) {
+        RecheioDePistache = recheioDePistache;
+    }
+}
